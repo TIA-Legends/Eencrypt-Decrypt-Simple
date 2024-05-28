79987471b7fcba0657cb672d89dfafb70db76091
0xodus <samuelmsuu@gmail.com>
2024-05-28 22:32:03 +0300

    feat: Add SimpleStringEncryptor contract for basic encryption and decryption
    This commit adds a new Solidity contract called SimpleStringEncryptor. It provides basic encryption and decryption functionalities by using a secret key to perform a simple XOR operation on each byte of the data.

---
+++ SimpleStringEncryptor.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+//This Solidity contract provides basic encryption and decryption functionalities. It uses a secret key to perform a simple XOR operation on each byte of the data. 
